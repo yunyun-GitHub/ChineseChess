54d2d3b6a8f72e6085d0819f3052a4943e3a4932
yunyun-GitHub <57121602+yunyun-GitHub@users.noreply.github.com>
2025-07-22 22:17:27 +0800

    Add files via upload

--- ceshi.py
+++ ceshi.py
@@ -16,8 +16,7 @@ class Game:
                       [0, 5, 0, 0, 0, 0, 0, 5, 0],
                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
                       [7, 6, 3, 2, 1, 2, 3, 6, 7]]
-        self.num_piece = {7: '红车', 6: '红马', 5: '红砲', 4: '红兵', 3: '红相', 2: '红仕', 1: '红帅',
-                          -7: '黑车', -6: '黑马', -5: '黑炮', -4: '黑卒', -3: '黑象', -2: '黑士', -1: '黑将'}
+        # 两组位置为图片左上角的像素坐标
         self.cols = [33, 93, 153, 214, 273, 333, 393, 453, 511]  # x轴对应列
         self.rows = [6, 65, 125, 186, 246, 306, 365, 426, 485, 544]  # y轴对应行
         # 初始化 Pygame
@@ -39,6 +38,8 @@ class Game:
         self.tracked_image = pygame.image.load("image/落子.png")  # 加载落子图片
         self.button_image = pygame.image.load("image/按钮.png")  # 按钮
 
+        self.num_piece = {7: '红车', 6: '红马', 5: '红砲', 4: '红兵', 3: '红相', 2: '红仕', 1: '红帅',
+                          -7: '黑车', -6: '黑马', -5: '黑炮', -4: '黑卒', -3: '黑象', -2: '黑士', -1: '黑将'}
         for key, value in self.num_piece.items():  # 加载所有棋子图片, 保存在字典中, 覆盖self.num_piece的值
             self.num_piece[key] = pygame.image.load("image/" + value + ".png")
 
@@ -51,23 +52,30 @@ class Game:
         self.running = False  # 游戏运行状态
         self.draw()
 
-    def update(self, event):
+    def update(self, pos):
         """更新游戏状态"""
-        coordinate = self.pos2coordinate(event.pos)
-        if coordinate[0] < 0:  # 点击了棋盘边缘
+        coordinate = self.pos2coordinate(pos)  # 坐标转换
+        row, col = coordinate
+
+        if row < 0:  # 点击了棋盘边缘
+            if row == -2:
+                print("点击了按钮", pos)
             self.checked = [-1, -1]  # 重置选中状态
             return
 
         if self.checked[0] < 0:  # 说明之前没有选中棋子
             # 乘积大于0表示符号相同, 说明点击的位置有棋子, 并且属于当前玩家
-            if self.board[coordinate[0]][coordinate[1]] * self.player > 0:
+            if self.pr[row][col] * self.player > 0:
                 self.checked = coordinate  # 记录选中坐标
             return
 
         # 说明之前已经选中了一个棋子，现在要移动
-        ar, ab, board, a = self.board2player([self.checked, coordinate])
+        (sr, sc), (er, ec) = self.checked, coordinate  # 解构元组, 如果两个坐标相同, 相当于没走
+        ar = [sr, sc, er, ec]
+        ab = [9 - sr, 8 - sc, 9 - er, 8 - ec]
 
-        if self.validate(a, board):  # 验证移动合法
+        a, b = (ar, self.pr) if self.player == 1 else (ab, self.pb)
+        if self.validate(a, b):  # 验证移动合法
             self.move(ar, ab)
             self.tracked = [self.checked, coordinate]
             self.player *= -1  # 轮到对方走棋
@@ -75,32 +83,41 @@ class Game:
 
     def move(self, ar, ab):
         """接受一个动作, 执行走棋"""
-        (sr, sc), (er, ec) = ar  # 解构元组, 如果两个坐标相同, 相当于没走
+        sr, sc, er, ec = ar  # 解构元组, 如果两个坐标相同, 相当于没走
         self.board[sr][sc], self.board[er][ec] = 0, self.board[sr][sc]
         self.pr[sr][sc], self.pr[er][ec] = 0, self.pr[sr][sc]
 
-        (sr, sc), (er, ec) = ab  # 解构元组, 如果两个坐标相同, 相当于没走
+        sr, sc, er, ec = ab  # 解构元组, 如果两个坐标相同, 相当于没走
         self.pb[sr][sc], self.pb[er][ec] = 0, self.pb[sr][sc]
 
-    def board2player(self, action):
-        (sr, sc), (er, ec) = action  # 解构元组, 如果两个坐标相同, 相当于没走
-        ar = [(sr, sc), (er, ec)]
-        sr, sc, er, ec = 9 - sr, 8 - sc, 9 - er, 8 - ec
-        ab = [(sr, sc), (er, ec)]
+    def draw(self):
+        """绘制所有内容"""
+        self.screen.blit(self.board_image, (0, 0))  # 画棋盘
 
-        if self.player == 1:
-            board = self.pr
-            a = ar
-        else:
-            board = self.pb
-            a = ab
+        # 接受一个棋盘二维列表, 遍历所有的元素和索引
+        for i, row in enumerate(self.board):
+            for j, val in enumerate(row):
+                if val != 0:
+                    piece_image = self.num_piece[val]  # 获取已经加载的图片
+                    self.screen.blit(piece_image, self.coordinate2pos([i, j]))  # 画棋子
 
-        return ar, ab, board, a
+        if self.checked[0] >= 0:  # 如果有棋子被选中, 就在该棋子处画上选中效果
+            self.screen.blit(self.checked_image, self.coordinate2pos(self.checked))
+        if self.tracked[0][0] >= 0:  # 画落子效果
+            self.screen.blit(self.tracked_image, self.coordinate2pos(self.tracked[0]))
+            self.screen.blit(self.tracked_image, self.coordinate2pos(self.tracked[1]))
+
+        # self.screen.blit(self.font.render("_红黑"[self.player] + "方走", True, (0, 0, 0)), (283, 297))  # 显示走棋棋手
+
+        self.screen.blit(self.button_image, (558, 290))  # 画上按钮效果
+        self.screen.blit(self.font.render("按钮", True, (180, 180, 180)), (570, 296))  # 显示按钮
+
+        pygame.display.flip()  # 更新显示
 
     @staticmethod
     def validate(action, board):
         """验证移动合法"""
-        (sr, sc), (er, ec) = action  # 解构元组
+        sr, sc, er, ec = action  # 解构元组
         value = board[sr][sc]
 
         mvs = []
@@ -167,16 +184,22 @@ class Game:
         棋盘坐标用线性代数矩阵方法表示, 从上到下为行, 从左到右为列
         """
 
-        # 查找列索引（对应棋盘坐标的列）
-        col_idx = bisect.bisect_right(self.cols, pos[0]) - 1
-        valid_col = col_idx >= 0 and self.cols[col_idx] <= pos[0] < self.cols[col_idx] + 56
+        x, y = pos  # 解构元组
 
-        # 查找行索引（对应棋盘坐标的行）
-        row_idx = bisect.bisect_right(self.rows, pos[1]) - 1
-        valid_row = row_idx >= 0 and self.rows[row_idx] <= pos[1] < self.rows[row_idx] + 56
+        # 查找索引, x横坐标对应列, y纵坐标对应行
+        col = bisect.bisect_right(self.cols, x) - 1
+        row = bisect.bisect_right(self.rows, y) - 1
 
-        # [-1, -1]表示点击了棋盘外
-        return [row_idx, col_idx] if valid_col and valid_row else [-1, -1]
+        left, up = self.cols[col], self.rows[row]
+        right, down = left + 56, up + 56
+
+        # 取值True或False代表是否有效, col >= 0 and row >= 0 的情况已包含在逻辑中
+        if left < x < right and up < y < down:
+            return [row, col]
+        elif 570 < x < 600 and 300 < y < 310:
+            return [-2, -2]  # 表示点击了按钮
+        else:
+            return [-1, -1]  # 表示点击了棋盘外
 
     def coordinate2pos(self, coordinate):
         """将棋盘坐标转换成屏幕像素坐标主要用于绘图
@@ -189,33 +212,12 @@ class Game:
         y = self.rows[row]
         return [x, y]
 
-    def draw(self):
-        """绘制所有内容"""
-        self.screen.blit(self.board_image, (0, 0))  # 画棋盘
-
-        # 接受一个棋盘二维列表, 遍历所有的元素和索引
-        for i, row in enumerate(self.board):
-            for j, val in enumerate(row):
-                if val != 0:
-                    piece_image = self.num_piece[val]  # 获取已经加载的图片
-                    self.screen.blit(piece_image, self.coordinate2pos([i, j]))  # 画棋子
-
-        if self.checked[0] >= 0:  # 如果有棋子被选中, 就在该棋子处画上选中效果
-            self.screen.blit(self.checked_image, self.coordinate2pos(self.checked))
-        if self.tracked[0][0] >= 0:  # 画落子效果
-            self.screen.blit(self.tracked_image, self.coordinate2pos(self.tracked[0]))
-            self.screen.blit(self.tracked_image, self.coordinate2pos(self.tracked[1]))
-
-        self.screen.blit(self.font.render("_红黑"[self.player] + "方走", True, (0, 0, 0)), (558, 293))  # 显示走棋棋手
-
-        pygame.display.flip()  # 更新显示
-
     def _handle_event(self, event: pygame.event.Event):
         """处理单个游戏事件"""
         if event.type == pygame.QUIT:
             self._quit_game()
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            self.update(event)
+            self.update(event.pos)
             self.draw()
 
     def _quit_game(self):
